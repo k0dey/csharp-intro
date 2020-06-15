eb8c715aa0dd0f7359662a263741ebb4118aa6c1
Kerry Parsons <kerryparsons1204@gmail.com>
2020-06-15 12:33:56 +0100

    first commit

---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace introduction
+
+{
+
+    class Mathematics
+    {
+        public int Addition(int numberOne, int numberTwo)
+        {
+            return numberOne + numberTwo;
+        }
+
+        public int Subtraction(int numberOne, int numberTwo)
+        {
+            return numberOne - numberTwo;
+        }
+
+        public int Multiplication(int numberOne, int numberTwo)
+        {
+            return numberOne * numberTwo;
+        }
+
+        public double Division (double numberOne, double numberTwo)
+        {
+            return numberOne / numberTwo;
+        }
+    }
+
+
+    class Program
+    {
+
+    
+        static void Main(string[] args)
+        {
+            Mathematics maths = new Mathematics();
+            int addMePlease = maths.Addition(1,2);
+            Console.WriteLine(addMePlease);
+            int subtractPlease = maths.Subtraction(10, 5);
+            Console.WriteLine(subtractPlease);
+
+           double dividePlease = maths.Division(10, 3);
+            Console.WriteLine(dividePlease);
+        }
+    }
+}
